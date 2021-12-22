7bf9079c866a7d811df4960eb7b21e56b9db00c6
Alex Munro <alexmunro91@gmail.com>
2021-12-22 05:14:49 +0000

    Day 21 part one

---
+++ spec/solutions/day21/day21_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "./solutions/day21/day21"
+
+RSpec.describe Day21 do
+  let(:day21) { described_class.new(4, 8) }
+
+  describe "#practice_game" do
+    subject { day21.practice_game }
+
+    it { is_expected.to eq 739_785 }
+  end
+
+  xdescribe "#quantum_game" do
+    subject { day21.quantum_game }
+
+    it { is_expected.to eq 444_356_092_776_315 }
+  end
+end
