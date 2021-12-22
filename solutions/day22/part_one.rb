33822eb584a7abba35182ac6d8b1bc9215e9d31c
Alex Munro <alexmunro91@gmail.com>
2021-12-22 05:55:37 +0000

    Day 22 part one

---
+++ solutions/day22/part_one.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative "./day22"
+puts "The answer to part one is #{Day22.part_one}"
---
+++ solutions/day22/part_two.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative "./day22"
+puts "The answer to part two is #{Day22.part_two}"
