d511a8dac6fa7ebd20fb9f9be271d020e14a1fd3
Alex Munro <alexmunro91@gmail.com>
2022-10-22 21:57:37 +0100

    Config tweaks

--- Gemfile
+++ Gemfile
@@ -6,9 +6,10 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 gem "advent_of_code_cli", "0.3.4", github: "AlexMunro/advent_of_code_cli"
 gem "debug"
+gem "matrix"
 gem "rbtree"
 gem "rspec"
 gem "rubocop"
 gem "rubocop-rspec"
 gem "solargraph"
-gem "matrix"
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 require "bundler/setup"
+require "debug"
 
 RSpec.configure do |config|
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
+  config.example_status_persistence_file_path =
+    "spec/examples.txt"
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
